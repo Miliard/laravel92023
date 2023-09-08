7d38795cedc39913a03526de4ee7d63848dd7b5c
wil-asus <elsalvadorazul@outlook.es>
2023-09-07 21:58:29 -0600

    capitulo 6

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome')->name('home');
+Route::view('/blog', 'blog')->name('blog');
+Route::view('/about', 'about')->name('about');
+Route::view('/contacto', 'contacto')->name('contact');
